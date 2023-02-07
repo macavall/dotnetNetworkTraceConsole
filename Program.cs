c0f324340fa77d8a10a9debec901c14df96e3a5c
macavall <43223084+macavall@users.noreply.github.com>
2023-02-07 12:59:04 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace deleteconsole563
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var semaphore = new SemaphoreSlim(10);
+
+            var tasks = new List<Task>();
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+                await semaphore.WaitAsync();
+
+                tasks.Add(MyMethod().ContinueWith((t) => semaphore.Release()));
+            }
+            await Task.WhenAll(tasks);
+            Console.WriteLine();
+        }
+        
+        public static async Task MyMethod()
+        {
+            await Task.Delay(6000);
+            Console.WriteLine("");
+        }
+    }
+}
